4855bc473b444f781ff9e667f2c51b96a3c4186d
Ломтев Михаил <ggsana@mail.ru>
2026-05-07 17:02:59 +0300

    add class'task'

---
+++ TaskTracker/TaskTracker/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTracker
+{
+    public class Task
+    {
+        public int Id {  get; set;}
+        public string Description { get; set;}
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Task() { }
+
+        public Task(int id,string descrip)
+        {
+            Id = id;
+            Description = descrip;
+            Status = "todo";    
+            CreatedAt = DateTime.Now;     
+            UpdatedAt = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {Description} [{Status}]";
+        }
+    }
+}
